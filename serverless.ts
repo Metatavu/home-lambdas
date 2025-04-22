2e3a1a19a3f4fd84298a7b5ef18d32b4619fe147
Kashif <muhammad.kashif18@ce.ceme.edu.pk>
2025-04-22 13:31:53 +0300

    Updated serverless.ts with necessary changes and cleanup

--- serverless.ts
+++ serverless.ts
@@ -24,7 +24,7 @@ import deleteSoftwareHandler from "@/functions/software-registry/delete-software
 import listUsersHandler from "@/functions/keycloak/list-users";
 import findUserHandler from "@/functions/keycloak/find-user";
 import updateUserAttributeHandler from "src/functions/keycloak/update-user-attributes";
-import removeUserAttributeHanndler from "src/functions/keycloak/remove-user-attribute";
+import removeUserAttributeHandler from "src/functions/keycloak/remove-user-attribute";
 import createQuestionnaireHandler from "@/functions/questionnaire/create-questionnaire";
 import findQuestionnaireHandler from "@/functions/questionnaire/find-questionnaire";
 import deleteQuestionnaireHandler from "src/functions/questionnaire/delete-questionnaire";
@@ -48,7 +48,6 @@ import updateVacationRequestHandler from "src/functions/vacation-request/update-
 import getResourceAllocationHandler from "src/functions/severa/get-resource-allocations-by-user";
 import getPhasesHandler from "src/functions/severa/get-phases-by-project";
 import getWorkHoursHandler from "src/functions/severa/get-filtered-workhours";
-// import vacationManagementHandler from "src/functions/vacation-management";
 import getkeyuserhandler from "src/functions/vacation-management/get-user";
 import getkeyusershandler from "src/functions/vacation-management/get-users";
 import updatevacationhandler from "src/functions/vacation-management/update-user-vacation";
@@ -147,14 +146,7 @@ const serverlessConfiguration: AWS = {
         statements: [
           {
             Effect: "Allow",
-            Action: ["s3:GetObject"],
-            Resource: isLocal
-              ? "*"
-              : "arn:aws:s3:::${opt:stage}-on-call-data/*",
-          },
-          {
-            Effect: "Allow",
-            Action: ["s3:PutObject"],
+            Action: ["s3:GetObject", "s3:PutObject"],
             Resource: isLocal
               ? "*"
               : "arn:aws:s3:::${opt:stage}-on-call-data/*",
@@ -205,7 +197,7 @@ const serverlessConfiguration: AWS = {
     listUsersHandler,
     findUserHandler,
     updateUserAttributeHandler,
-    removeUserAttributeHanndler,
+    removeUserAttributeHandler,
     createQuestionnaireHandler,
     findQuestionnaireHandler,
     deleteQuestionnaireHandler,
@@ -229,7 +221,6 @@ const serverlessConfiguration: AWS = {
     getResourceAllocationHandler,
     getPhasesHandler,
     getWorkHoursHandler,
-    // vacationManagementHandler,
     getkeyuserhandler,
     getkeyusershandler,
     updatevacationhandler,

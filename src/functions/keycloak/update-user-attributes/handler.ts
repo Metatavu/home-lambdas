74d7d74b8829f272402bf0447a94e5a895ec3bdd
Aleksandra Balashova <ext-aleksandra.balashova@metatavu.fi>
2025-04-29 13:39:59 +0300

    Fix email parsing in updateUserAttributeHandler

--- src/functions/keycloak/update-user-attributes/handler.ts
+++ src/functions/keycloak/update-user-attributes/handler.ts
@@ -16,7 +16,7 @@ const updateUserAttributeHandler: APIGatewayProxyHandler = async (event: APIGate
     }
 
     const body = JSON.parse(JSON.stringify(event.body));
-    const email = body;
+    const email = body.email;
     const { id, attributeName } = event.pathParameters ?? {};
 
     const allowedAttributes = ["isSeveraOptIn"];

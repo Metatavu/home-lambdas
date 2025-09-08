55b69be0826473e95193a000274af08495f0fbef
Vladimir Rybin <vladimirrybin2003@gmail.com>
2025-09-08 16:11:37 +0300

    implement function onCallImportFromJsonHandler

--- src/functions/on-call/create-on-call-data-from-json/index.ts
+++ src/functions/on-call/create-on-call-data-from-json/index.ts
@@ -3,12 +3,20 @@ import { handlerPath } from "@libs/handler-resolver";
 export default {
 	handler: `${handlerPath(__dirname)}/handler.main`,
 	events: [
-        {
+		{
 			httpApi: {
 				method: 'post',
 				path: '/on-call/import-json',
 				authorizer: {
 					name: "homeKeycloakAuthorizer"
+				},
+				request: {
+					parameters: {
+						querystrings: {
+							year: true
+						}
+					},
+					contentType: 'application/json'
 				}
 			}
 		}

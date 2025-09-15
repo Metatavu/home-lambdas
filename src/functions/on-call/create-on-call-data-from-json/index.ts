7b5557dbd3ff41c4e7d9d67db8e36c129739fd19
Vladimir Rybin <vladimirrybin2003@gmail.com>
2025-09-15 10:41:42 +0300

    AWS V3 refactoring, endpoint refactoing, linter

--- src/functions/on-call/create-on-call-data-from-json/index.ts
+++ src/functions/on-call/create-on-call-data-from-json/index.ts
@@ -1,24 +1,16 @@
 import { handlerPath } from "@libs/handler-resolver";
 
 export default {
-	handler: `${handlerPath(__dirname)}/handler.main`,
-	events: [
-		{
-			httpApi: {
-				method: 'post',
-				path: '/on-call/import-json',
-				authorizer: {
-					name: "homeKeycloakAuthorizer"
-				},
-				request: {
-					parameters: {
-						querystrings: {
-							year: true
-						}
-					},
-					contentType: 'application/json'
-				}
-			}
-		}
-	]
+  handler: `${handlerPath(__dirname)}/handler.main`,
+  events: [
+    {
+      httpApi: {
+        method: "post",
+        path: "/on-call/import-json",
+        authorizer: {
+          name: "homeKeycloakAuthorizer",
+        },
+      },
+    },
+  ],
 };

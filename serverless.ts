07166540e45f6f12f1718a4d625535ab7484b914
Vladimir Rybin <vladimirrybin2003@gmail.com>
2025-09-30 15:43:02 +0300

    use node 20

--- serverless.ts
+++ serverless.ts
@@ -74,7 +74,7 @@ const serverlessConfiguration: AWS = {
   ],
   provider: {
     name: "aws",
-    runtime: "nodejs18.x",
+    runtime: "nodejs20.x",
     region: region,
     deploymentBucket: {
       name: isLocal
@@ -236,7 +236,7 @@ const serverlessConfiguration: AWS = {
       minify: false,
       sourcemap: true,
       exclude: ["aws-sdk"],
-      target: "node18",
+      target: "node20",
       define: { "require.resolve": undefined },
       platform: "node",
       concurrency: 10,

a2a90c2b047ed0aa56ca9056b8f1d8011700ef0d
JuusoHeikkinen02 <juuso.heikkinen02@gmail.com>
2025-10-01 13:36:01 +0300

    fixed the isAdminUser functions failure that was caused by not returning the needed data from the token.

--- src/libs/auth-utils.ts
+++ src/libs/auth-utils.ts
@@ -44,7 +44,8 @@ export const getAuthDataFromToken = (event: { headers: APIGatewayProxyEvent['hea
     console.log('Decoded JWT Token:', JSON.stringify(decodedToken, null, 2));
 
     return {
-      sub: decodedToken.sub
+      sub: decodedToken.sub,
+      realm_access: decodedToken.realm_access
     };
   } catch (error) {
     console.error('Error decoding JWT token:', error);

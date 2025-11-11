efefa66aecb34947fcddbfb18c3bdb2223532d35
Alexander Kerr <89669088+Alexander-K-byte@users.noreply.github.com>
2025-11-11 10:23:18 +0200

    Update src/types/severa/workDays/severaResponseWorkDays.ts
    Co-authored-by: Josh Dallos <78350882+Jdallos@users.noreply.github.com>

--- src/types/severa/workDays/severaResponseWorkDays.ts
+++ src/types/severa/workDays/severaResponseWorkDays.ts
@@ -1,7 +1,5 @@
 /**
  * Interface for Severa Response Work Days.
- * date and isHoliday fields added so that function can find expected work week
- * for the logged in user
  */
 interface SeveraResponseWorkDays {
   date: string;

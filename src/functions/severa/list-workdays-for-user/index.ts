69efdce62c830818dc084a5f48e23050b8b1a2ed
JuusoHeikkinen02 <juuso.heikkinen02@gmail.com>
2025-11-12 09:15:54 +0200

    Added the leading / to the path of the handler

--- src/functions/severa/list-workdays-for-user/index.ts
+++ src/functions/severa/list-workdays-for-user/index.ts
@@ -6,7 +6,7 @@ export default {
     {
       httpApi: {
         method: "get",
-        path: "severa/users/{severaUserId}/workDays",
+        path: "/severa/users/{severaUserId}/workdays",
         authorizer: {
           name: "homeKeycloakAuthorizer"
         }

6006335d6988db84349a2d2436374bca18358e5d
Alexander-K-byte <alexanderkerr@hotmail.com>
2025-12-10 16:00:12 +0200

    updated to only use env value

--- serverless.ts
+++ serverless.ts
@@ -111,7 +111,7 @@ const serverlessConfiguration: AWS = {
       SEVERA_CLIENT_SECRET: env.SEVERA_CLIENT_SECRET,
       DYNAMODB_ENDPOINT: isLocal ? "http://localhost:8000" : undefined,
       CHANNEL_ID: env.CHANNEL_ID,
-      HOME_BUCKET_NAME: env.HOME_BUCKET_NAME_DEV || "local-bucket",
+      HOME_BUCKET_NAME: env.HOME_BUCKET_NAME_DEV,
       HOME_BUCKET_REGION: region,
       ADMIN_SLACK_USERS: env.ADMIN_SLACK_USERS || undefined,
       ADMIN_EMAILS: env.ADMIN_EMAILS || undefined,

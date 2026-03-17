618fce9beb7d9229c416b2aab06beb89e1eb14d0
shiva <shivaadhikari0214@gmail.com>
2026-03-17 13:41:07 +0200

    feature production ready

--- src/functions/users/flextime/index.ts
+++ src/functions/users/flextime/index.ts
@@ -1,17 +1,22 @@
 import { handlerPath } from "@libs/handler-resolver";
 
-export default {
-  handler: `${handlerPath(__dirname)}/handler.main`,
-  timeout: 30,
-  events: [
-    {
-      httpApi: {
-        method: "get",
-        path: "/severa/users/flextime",
-        authorizer: {
-          name: "homeKeycloakAuthorizer"
+// NOTE: This lambda is marked as inactive in the OpenAPI spec (x-status: inactive).
+const isInactive = true;
+
+export default isInactive
+  ? {}
+  : {
+      handler: `${handlerPath(__dirname)}/handler.main`,
+      timeout: 30,
+      events: [
+        {
+          httpApi: {
+            method: "get",
+            path: "/severa/users/flextime",
+            authorizer: {
+              name: "homeKeycloakAuthorizer"
+            }
+          }
         }
-      }
-    }
-  ]
-};
+      ]
+    };

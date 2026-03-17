1e9eb0449148876fb4dad68b9516ca178817cdf9
shiva <shivaadhikari0214@gmail.com>
2026-03-17 13:31:32 +0200

    added unnecessary comments on the end.

--- src/functions/keycloak/list-users/index.ts
+++ src/functions/keycloak/list-users/index.ts
@@ -1,21 +1,16 @@
 import { handlerPath } from "src/libs/handler-resolver";
 
-// NOTE: This lambda is marked as inactive in the OpenAPI spec (x-status: inactive).
-const isInactive = false;
-
-export default isInactive
-  ? {}
-  : {
-      handler: `${handlerPath(__dirname)}/handler.main`,
-      events: [
-        {
-          httpApi: {
-            method: "get",
-            path: "/users",
-            authorizer: {
-              name: "homeKeycloakAuthorizer"
-            }
-          }
+export default {
+  handler: `${handlerPath(__dirname)}/handler.main`,
+  events: [
+    {
+      httpApi: {
+        method: "get",
+        path: "/users",
+        authorizer: {
+          name: "homeKeycloakAuthorizer"
         }
-      ]
-    };
+      }
+    }
+  ]
+};

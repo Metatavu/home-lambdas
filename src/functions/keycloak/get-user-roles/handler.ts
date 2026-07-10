afd537e86ffb07a7d8efcb3be00251a6faa3a12d
NeonValtokari <neon.valtokari@tuni.fi>
2026-07-10 13:36:49 +0300

    Fix build errors

--- src/functions/keycloak/get-user-roles/handler.ts
+++ src/functions/keycloak/get-user-roles/handler.ts
@@ -1,6 +1,6 @@
 import type { APIGatewayProxyEvent, APIGatewayProxyHandler } from "aws-lambda";
-import { CreateKeycloakApiService } from "src/database/services/keycloak-api-service";
 import { middyfy } from "src/libs/lambda";
+import { CreateKeycloakApiService } from "src/services/keycloak-api-service";
 
 const getUserRolesHandler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent) => {
   try {
